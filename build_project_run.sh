23e8bc793e3ef71f9fe21ec71b67e22323984e0e
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-05-14 23:40:47 -0300

    IMP: include game directory in the build script

--- build_project_run.sh
+++ build_project_run.sh
@@ -4,5 +4,8 @@ gcc -o bin/Debug/TF  ./src/main.c \
 -I./include/ \
 ./include/drivers/keyboard/keyboard.c -I./include/drivers/keyboard \
 ./include/drivers/graphic/graphic.c -I./include/drivers/graphic \
+./include/game/player/player.c -I./include/game/player \
+./include/game/game.c -I./include/game/ \
+ -I./include/game/ \
 ./bin/Debug/libraylib.a -I./build/external/raylib-master/src \
 -lm && cd bin/Debug && ./TF && cd ../.. 
