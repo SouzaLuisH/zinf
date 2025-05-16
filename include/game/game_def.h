ab05c3757c9a66f6cabacf3ace90ee11d7beaba1
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-05-15 22:18:21 -0300

    CHANGE: enums of struct

--- include/game/game_def.h
+++ include/game/game_def.h
@@ -6,7 +6,8 @@
 #define DEBUG_PRINTS true
 
 //----- GAME DEFINES ----//
-#define DEFAULT_PLAYER_VELOCITY 20 //pixels per frame 
+#define DEFAULT_PLAYER_VELOCITY 50 //pixels per frame 
+#define MAX_ENEMIES_PER_LEVEL 5
 
 //---- GAME STRUTURES ----///
 typedef struct {
@@ -14,15 +15,32 @@ typedef struct {
     float y;
 } Vector2D;
 
+typedef enum {
+    SOUTH = 0,
+    NORTH = 1,
+    WEST  = 2,
+    EAST  = 3
+} Orientation;
+
 typedef struct {
     Vector2D position;
-    enum {south= 0, north=1, west = 2, east = 3} orientation;
+    Orientation orientation;
     int score;
     int lives;
     bool hasWeapon;
     bool isWeaponActive;
     //warpon type
 }Player;
+
+typedef struct {
+    Vector2D position;
+    Orientation orientation;
+    int lives;
+    int reward;
+}Enemies;
+
+
+
 
 // --------------- FUNCTION HEADERS --------//
 int testegame(char move);
