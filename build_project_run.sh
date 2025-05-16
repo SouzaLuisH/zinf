bd533c3fda8845a9050ab4557131032a509384c7
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-05-15 22:19:35 -0300

    IMP: add enemies archive in build script

--- build_project_run.sh
+++ build_project_run.sh
@@ -5,6 +5,7 @@ gcc -o bin/Debug/TF  ./src/main.c \
 ./include/drivers/keyboard/keyboard.c -I./include/drivers/keyboard \
 ./include/drivers/graphic/graphic.c -I./include/drivers/graphic \
 ./include/game/player/player.c -I./include/game/player \
+./include/game/enemies/enemies.c -I./include/game/enemies \
 ./include/game/game.c -I./include/game/ \
  -I./include/game/ \
 ./bin/Debug/libraylib.a -I./build/external/raylib-master/src \
