10458d3c96c0114cd409bf028b48d9ef9b78e5f8
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-06-07 19:45:48 -0300

    IMP:sei la

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
 #include "resource_dir.h" // utility header for SearchAndSetResourceDir
-#include <stdio.h>
 #include "keyboard.h"
 #include "graphic.h"
+#include <game_def.h>
 
-#define WINDOW_WIDHT 1000
-#define WINDOW_HEIGHT 500
+#define WINDOW_WIDHT 1200
+#define WINDOW_HEIGHT 800
 
 //------------------------ Global Var--------------------------------//
 enum game_mode_options
@@ -17,9 +19,8 @@ enum game_mode_options
 	quit
 };
 
-int global_press_key = 0;
+unsigned char global_press_key = 0;
 enum game_mode_options global_game_screen = main_menu;
-
 
 //------------------------ Menu Functions--------------------------------//
 
@@ -63,9 +64,6 @@ int main_menu_f()
 
 //-------------------------- Game Function -------------------------------//
 
-float x = 0;
-float y = 0;
-
 void in_game_f()
 {
 	static bool is_paused = false;
@@ -89,27 +87,13 @@ void in_game_f()
 	}
 	else
 	{
-		switch (global_press_key)
+		if (global_press_key == 'p')
 		{
-		case 'w':
-			y -= 20;
-			break;
-		case 's':
-			y += 20;
-			break;
-		case 'a':
-			x -= 20;
-			break;
-		case 'd':
-			if (x + 1 < WINDOW_WIDHT - 20)
-				x += 20;
-			break;
-		case 'p':
 			is_paused = true;
-			break;
 		}
+
+		game_loop(global_press_key);
 		driver_print_text("-> [P]: pause ", WINDOW_WIDHT - 300, WINDOW_HEIGHT - 50, 0);
-		DrawRectangle(x, y, 20, 20, RED);
 	}
 }
 
@@ -118,9 +102,9 @@ int main()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 	InitWindow(WINDOW_WIDHT, WINDOW_HEIGHT, "ZINF");
 	SetExitKey(0);
-
+	srand(32);
 	// SearchAndSetResourceDir("resources");
-
+	init_game_data();
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
@@ -149,6 +133,62 @@ int main()
 		EndDrawing();
 	}
 
+	finish_game();
 	CloseWindow();
 	return 0;
 }
+
+/*
+	Mapa:
+	 - Leitura de arquivos txt
+	 - criar matriz preenchida a partir deles OK
+	 - criar estrutura para os elementos iniciais do jogo OK
+	 - Plotar o mapa OK
+	 - Imagens
+	 - (extra) Mexer na camera
+	 - (extra) animacaoes
+	Inimigos
+	 - Movimentação aleátoria
+	 - Pontuação OK
+	 - Sistema de vidas OK
+	 - Imagens
+	 - (extra) inimigos mais inteligentes
+	- (extra) animacaoes
+
+	Jogador:
+	 - Posicao Inicial OK
+	 - Movimentacao OK
+	 - Orientacao
+	 - Ativacao Espada OK
+	 - Pontuacao OK
+	 - Vidas OK
+	 - Itens OK
+	 - Imagens
+	 - (extra) animacaoes
+
+	Logica:
+		- Colisao com o mapa OK
+		- Ataque do inimigo OK
+		- Ataque do jogador OK
+			* ataque com espada
+		- Pontuação OK
+		- Vidas perder/ganhar OK
+		- Passar de fase
+	Driver:
+		- Leitura do Teclado  OK
+		- Desenho do mapa  OK
+		- Desenhar personagens OK*
+		- (extra) Animacoes
+		- (extra) camera
+	Menu:
+		- Menu principal OK
+		- Menu Pause OK
+		- Ranking
+			* Leitura/edição de arquivo.bin
+		- (extra) salvar jogo
+
+
+
+
+
+*/
