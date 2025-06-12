e33136194eb5c22c572b00e0a2aa644be43caf62
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-06-11 23:36:12 -0300

    IMP: use of keyboard bit_mask to make player moves in diagonal

--- include/game/game.c
+++ include/game/game.c
@@ -1,76 +1,46 @@
-#include "game_def.h"
-#include "player.h"
-#include "enemies.h"
-#include "graphic.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+#include "player.h"
+#include "keyboard.h"
+#include "enemies.h"
+#include "graphic.h"
+
+#include "game_def.h"
 
 Player Link;
 Game_State Map_Data;
 
 // ----------------- TEMPORARIO ----------------//
-/* 
-char archive[16][24] = {
-    "     M        PPPP      ",
-    "              M P       ",
-    "               P        ",
-    "       M    V PP        ",
-    "          PPPP          ",
-    "                 M      ",
-    "              M         ",
-    "                        ",
-    "       J   P            ",
-    "   M      P             ",
-    "         E P            ",
-    "PPPPPPPPPPPP            ",
-    "                        ",
-    "              M         ",
-    "                       V",
-    "                        ",
-};
-*/
 
-char proto_mapa (char map[MAP_HEIGHT][MAP_WIDTH], char *arq_nome)
+char proto_mapa(char map[MAP_HEIGHT][MAP_WIDTH], char *arq_nome)
 {
-    int i=0, j=0;
-    FILE *arq_map=fopen(arq_nome, "r");
-    if (arq_map==NULL){
-        printf("ERRO! O mapa está com algum problema.");
-        return 0;
+    int i = 0, j = 0;
+    FILE *arq_map = fopen(arq_nome, "r");
+    if (arq_map == NULL)
+    {
+        if (DEBUG_PRINTS)
+        {
+            printf("ERRO! O mapa está com algum problema.");
+        }
+        return 1;
     }
-    
-    for(i=0;i<MAP_HEIGHT;i++){
-        while(j<MAP_WIDTH){
-            char cont=fgetc(arq_map);
 
-            if (cont !='\r' && cont !='\n' && cont != '\0'){
-                map[i][j]=cont;
+    for (i = 0; i < MAP_HEIGHT; i++)
+    {
+        while (j < MAP_WIDTH)
+        {
+            char cont = fgetc(arq_map);
+
+            if (cont != '\r' && cont != '\n' && cont != '\0')
+            {
+                map[i][j] = cont;
                 j++;
             }
         }
-        j=0;
-    }
-        
-
-}
-
-int get_color_for_map_char(char c)
-{
-    switch (c)
-    {
-    case ' ':
-        return MAP_COLOR_EMPTY;
-    case 'M':
-        return MAP_COLOR_MONSTER;
-    case 'E':
-        return MAP_COLOR_WEAPON;
-    case 'V':
-        return MAP_COLOR_LIFE;
-    case 'P':
-        return MAP_COLOR_WALL;
-    default:
-        return MAP_COLOR_UNKNOWN;
+        j = 0;
     }
 }
 
@@ -81,7 +51,7 @@ void draw_map(Game_State *state, int width, int height)
         if (state->lives[i].isEnable)
         {
 
-            driver_draw_square(state->lives[i].position.x, state->lives[i].position.y, ITEM_SIZE, MAP_COLOR_LIFE);
+            driver_draw_square(state->lives[i].position.x, state->lives[i].position.y, TILE_SIZE, MAP_COLOR_LIFE);
         }
     }
 
@@ -89,13 +59,13 @@ void draw_map(Game_State *state, int width, int height)
     {
         if (state->monsters[i].isEnable)
         {
-            driver_draw_square(state->monsters[i].position.x, state->monsters[i].position.y, ITEM_SIZE, MAP_COLOR_MONSTER);
+            driver_draw_square(state->monsters[i].position.x, state->monsters[i].position.y, TILE_SIZE, MAP_COLOR_MONSTER);
         }
     }
 
     for (int i = 0; i < state->n_walls; i++)
     {
-        driver_draw_square(state->walls[i].x, state->walls[i].y, ITEM_SIZE, MAP_COLOR_WALL);
+        driver_draw_square(state->walls[i].x, state->walls[i].y, TILE_SIZE, MAP_COLOR_WALL);
     }
 
     for (int i = 0; i < state->n_weapons; i++)
@@ -103,7 +73,7 @@ void draw_map(Game_State *state, int width, int height)
         if (state->weapons[i].isEnable)
         {
 
-            driver_draw_square(state->weapons[i].position.x, state->weapons[i].position.y, ITEM_SIZE, MAP_COLOR_WEAPON);
+            driver_draw_square(state->weapons[i].position.x, state->weapons[i].position.y, TILE_SIZE, MAP_COLOR_WEAPON);
         }
     }
 }
@@ -118,8 +88,8 @@ void fill_wall_positions(Vector2D *position, char map[][MAP_WIDTH], int width, i
         {
             if (map[i][j] == target_char)
             {
-                position[index].x = j * ITEM_SIZE;
-                position[index].y = i * ITEM_SIZE;
+                position[index].x = j * TILE_SIZE;
+                position[index].y = i * TILE_SIZE;
                 index++;
             }
         }
@@ -137,15 +107,15 @@ void fill_elements_init_data(Element *elements, char map[][MAP_WIDTH], int width
             if (map[i][j] == target_char)
             {
                 elements[index].isEnable = true;
-                elements[index].position.x = j * ITEM_SIZE;
-                elements[index].position.y = i * ITEM_SIZE;
+                elements[index].position.x = j * TILE_SIZE;
+                elements[index].position.y = i * TILE_SIZE;
                 index++;
             }
         }
     }
 }
 
-//-----------------------------------------//
+//--------------------------------------------//
 
 void get_initial_position_of_all_elements(Game_State *map, char map_char[][MAP_WIDTH], int width, int height)
 {
@@ -205,27 +175,40 @@ void free_all_elements(Game_State *map)
 
 //-------- Colision check function------------------------//
 
-void get_player_movement_coords(float *x, float *y, char key_pressed)
-{
-    *x = key_pressed == 'd' ? 1.00 : key_pressed == 'a' ? -1.00
-                                                        : 0;
-    *y = key_pressed == 'w' ? -1.00 : key_pressed == 's' ? 1.00
-                                                         : 0;
-}
-
 bool check_wall_colision(Vector2D position, Game_State *map)
 {
+    const float HITBOX_OFFSET = (TILE_SIZE - PLAYER_HITBOX_SIZE) / 2.0f;
+
+    // Define a área de colisão (hitbox) do jogador, centralizada
+    float player_hitbox_x = position.x;
+    float player_hitbox_y = position.y;
+    float player_hitbox_width = PLAYER_HITBOX_SIZE;
+    float player_hitbox_height = PLAYER_HITBOX_SIZE;
+
     for (int i = 0; i < map->n_walls; i++)
     {
-        if (position.x == map->walls[i].x && position.y == map->walls[i].y)
+        float wall_x = map->walls[i].x;
+        float wall_y = map->walls[i].y;
+        float wall_width = TILE_SIZE;
+        float wall_height = TILE_SIZE;
+
+        // Verifica colisão AABB entre o jogador e a parede
+        if (player_hitbox_x < wall_x + wall_width &&
+            player_hitbox_x + player_hitbox_width > wall_x &&
+            player_hitbox_y < wall_y + wall_height &&
+            player_hitbox_y + player_hitbox_height > wall_y)
         {
             return true;
         }
     }
 
-    if (position.x < 0 || position.x > MAP_WIDTH * ITEM_SIZE)
+    // Verifica colisão com as bordas do mapa
+    float map_width_pixels = MAP_WIDTH * TILE_SIZE;
+    float map_height_pixels = MAP_HEIGHT * TILE_SIZE;
+
+    if (player_hitbox_x < 0 || player_hitbox_x + player_hitbox_width > map_width_pixels)
         return true;
-    if (position.y < 0 || position.y > MAP_HEIGHT * ITEM_SIZE)
+    if (player_hitbox_y < 0 || player_hitbox_y + player_hitbox_height > map_height_pixels)
         return true;
 
     return false;
@@ -275,7 +258,7 @@ bool check_monster_player_colision(Vector2D position, int *monster_position, Gam
 
 //----------handle elements functions -----//
 
-void handle_extra_lifes(Player *player, Game_State *map, char key_pressed)
+void handle_extra_lifes(Player *player, Game_State *map)
 {
     int life_position = 0;
 
@@ -291,7 +274,7 @@ void handle_extra_lifes(Player *player, Game_State *map, char key_pressed)
     }
 }
 
-void handle_weapon_elements(Player *player, Game_State *map, char key_pressed)
+void handle_weapon_elements(Player *player, Game_State *map)
 {
     int weapon_position = 0;
 
@@ -307,23 +290,40 @@ void handle_weapon_elements(Player *player, Game_State *map, char key_pressed)
     }
 }
 
-void handle_player_movement(Player *player, Game_State *map, char key_pressed)
+void handle_player_movement(Player *player, Game_State *map, uint8_t key_pressed)
 {
 
     float x_component = 0.00;
     float y_component = 0.00;
 
-    get_player_movement_coords(&x_component, &y_component, key_pressed);
+    if ((key_pressed & KEY_BIT_W) || (key_pressed & KEY_BIT_UP))
+    {
+        y_component = -1.00f;
+    }
+    else if ((key_pressed & KEY_BIT_S) || (key_pressed & KEY_BIT_DOWN))
+    {
+        y_component = 1.00f;
+    }
+
+    if ((key_pressed & KEY_BIT_D) || (key_pressed & KEY_BIT_RIGHT))
+    {
+        x_component = 1.00f;
+    }
+    else if ((key_pressed & KEY_BIT_A) || (key_pressed & KEY_BIT_LEFT))
+    {
+        x_component = -1.00f;
+    }
 
     if (x_component == 0 & y_component == 0)
         return;
 
-    Vector2D new_player_pos = {player->position.x + x_component * DEFAULT_PLAYER_VELOCITY,
-                               player->position.y + y_component * DEFAULT_PLAYER_VELOCITY};
+    // TODO, normalize vector if player is moving in diagonal
+    Vector2D new_player_pos = {player->position.x + x_component * DEFAULT_PLAYER_VELOCITY * 0.016,
+                               player->position.y + y_component * DEFAULT_PLAYER_VELOCITY * 0.016};
 
     if (check_wall_colision(new_player_pos, map) == false)
     {
-        move_player(player, key_pressed);
+        move_player(player, &new_player_pos);
     }
 }
 
@@ -359,40 +359,70 @@ void handle_player_monster_interation(Player *player, Game_State *map)
     }
 }
 
-//------------------------------------//
+//-------handle check function -----//
 
-int init_game_data(char stage_no)
+bool check_win_condition(Game_State *map)
 {
-    // TODO read map file, to set the positions of the enemies and player
-    // TODO save map in matrix
-    char arq_path[100]={0};
-    sprintf(arq_path, "../../include/game/mapa_%c.txt", stage_no);
+    for (int i = 0; i < map->n_monsters; i++)
+    {
+        if (map->monsters[i].isEnable == true)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool check_user_active_weapon(uint8_t input)
+{
+    if (input & KEY_BIT_J)
+    {
+        return true;
+    }
+    return false;
+}
+//--------------------------------------------
+
+int init_game_data(int stage_no, bool keep_weapon)
+{
+    char arq_path[50] = {0};
+    sprintf(arq_path, "../../include/game/maps/mapa_%d.txt", stage_no);
     char archive[MAP_HEIGHT][MAP_WIDTH];
 
-    
-    printf("============\n %s \n ============", arq_path);
-    proto_mapa(archive, arq_path);
-    player_init_status(&Link);
+    if (DEBUG_PRINTS)
+    {
+        printf("============\n Map Path: %s \n ============", arq_path);
+    }
+    proto_mapa(archive, arq_path); // TODO get the value to check the end of the game
+    player_init_status(&Link, keep_weapon);
     get_player_initial_position(&Link, archive, MAP_HEIGHT, MAP_WIDTH);
     get_initial_position_of_all_elements(&Map_Data, archive, MAP_WIDTH, MAP_HEIGHT);
-
 
     if (DEBUG_PRINTS)
     {
         printf("\nMonstros: %d, Vidas: %d, Armas: %d, Paredes: %d\n",
                Map_Data.n_monsters, Map_Data.n_lives, Map_Data.n_weapons, Map_Data.n_walls);
     }
-    return 1;
+    return 0;
 }
 
-
-void game_loop(char move)
+void game_loop()
 {
-    handle_player_movement(&Link, &Map_Data, move);
-    handle_extra_lifes(&Link, &Map_Data, move);
-    handle_weapon_elements(&Link, &Map_Data, move);
+    uint8_t keys_read = 0;
+    read_keyboard(&keys_read, true);
+
+    handle_player_movement(&Link, &Map_Data, keys_read);
+    handle_extra_lifes(&Link, &Map_Data);
+    handle_weapon_elements(&Link, &Map_Data);
     //---
-    handle_player_weapon(&Link, move);
+
+    if (check_win_condition(&Map_Data))
+    {
+        free_all_elements(&Map_Data);
+        init_game_data(2, true); // TODO, incrementar 1 mapa
+    }
+
+    handle_player_weapon(&Link, check_user_active_weapon(keys_read));
     handle_player_monster_interation(&Link, &Map_Data);
     draw_map(&Map_Data, MAP_WIDTH, MAP_HEIGHT);
     draw_player(&Link);
@@ -402,5 +432,5 @@ int finish_game()
 {
     free_all_elements(&Map_Data);
 
-    return 1;
+    return 0;
 }
