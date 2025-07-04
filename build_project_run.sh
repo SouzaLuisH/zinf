693386ea025315215fb8ff475fa6cd7e8d45b490
Luis Henrique de Souza <luishenriquedesouza112@gmail.com>
2025-07-03 21:14:10 -0300

    IMP: improving build and IDE settings

--- build_project_run.sh
+++ build_project_run.sh
@@ -4,6 +4,7 @@ gcc -o bin/Debug/TF  ./src/main.c \
 -I./include/ \
 ./include/drivers/keyboard/keyboard.c -I./include/drivers/keyboard \
 ./include/drivers/graphic/graphic.c -I./include/drivers/graphic \
+./include/drivers/archive/archive.c -I./include/drivers/archive \
 ./include/game/player/player.c -I./include/game/player \
 ./include/game/enemies/enemies.c -I./include/game/enemies \
 ./include/game/game.c -I./include/game/ \
